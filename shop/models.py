ea4777d5afdd5f1e73d76d8f8396587dc3240060
KaranDevnani1 <146174339+KaranDevnani1@users.noreply.github.com>
2024-08-27 21:48:52 +0530

    Add files via upload

---
+++ shop/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import AbstractUser,Group,Permission
+from django.utils.translation import gettext as _
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products',
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
+class CustomUser(AbstractUser):
+    is_staff = models.BooleanField(default=False)
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    # Add related_name to avoid conflicts
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_('groups'),
+        blank=True,
+        help_text=_(
+            'The groups this user belongs to. A user will get all permissions '
+            'granted to each of their groups.'
+        ),
+        related_name='customuser_set'  # You can choose any unique name you prefer
+    )
+    
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        help_text=_('Specific permissions for this user.'),
+        related_name='customuser_set'  # You can choose any unique name you prefer
+    )
